35991f7dff9849babf3a27049b9744b1cab035fa
Reg Tiangha <rtiangha@users.noreply.github.com>
2025-05-24 09:31:22 -0600

    fix typo

--- src/org/smssecure/smssecure/mms/AttachmentManager.java
+++ src/org/smssecure/smssecure/mms/AttachmentManager.java
@@ -34,7 +34,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import org.junit.assert;
+import org.junit.Assert;
 
 import org.smssecure.smssecure.MediaPreviewActivity;
 import org.smssecure.smssecure.R;
